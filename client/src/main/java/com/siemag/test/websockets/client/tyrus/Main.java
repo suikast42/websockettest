a350926b5645914f321e8195967c80038d3911c3
suikast42 <S_Vurucu@hotmail.com>
2014-04-23 11:00:16 +0200

    Recnonnection works with grizyl. But with SocketException after 3 server restarts

--- client/src/main/java/com/siemag/test/websockets/client/tyrus/Main.java
+++ client/src/main/java/com/siemag/test/websockets/client/tyrus/Main.java
@@ -2,13 +2,17 @@ package com.siemag.test.websockets.client.tyrus;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.glassfish.grizzly.threadpool.ThreadPoolConfig;
 import org.glassfish.tyrus.client.ClientManager;
+import org.glassfish.tyrus.container.grizzly.client.GrizzlyClientContainer;
+import org.glassfish.tyrus.container.grizzly.client.GrizzlyClientSocket;
 
 import javax.websocket.DeploymentException;
+import javax.websocket.Session;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author: vuru
@@ -17,24 +21,32 @@ import java.util.concurrent.CountDownLatch;
  */
 public class Main {
 
-
-    private static CountDownLatch latch;
+//    http://www.programmingforliving.com/2013/08/jsr-356-java-api-for-websocket-client-api.html
     private static Logger logger = Logger.getLogger(Main.class);
     public static void main(String[] args) {
         BasicConfigurator.configure();
-        latch = new CountDownLatch(1);
 
         try {
             ClientManager client = ClientManager.createClient();
             client.getProperties().put(ClientManager.RECONNECT_HANDLER,new TyrusReconnectHandler()) ;
-            client.getScheduledExecutorService().shutdownNow();
+
+
+//            client.getScheduledExecutorService().shutdownNow();
             WebsocketClientGirzly socket = new WebsocketClientGirzly();
-            client.connectToServer(socket, new URI("ws://localhost:8080/server/hello/1"));
+             client.connectToServer(socket, new URI("ws://localhost:8080/server/hello/1"));
 //            latch.await();
 
             int i=0;
             while(true){
-                socket.send(String.valueOf(i));
+                try {
+                    String text = String.valueOf(i);
+                    logger.info("Try to send "+text);
+                    socket.send(text);
+                    logger.info("Send "+text);
+                } catch (Exception e) {
+                    logger.error("CAN'T SEND",e);
+                  // IGNORE
+                }
                 i++;
                 Thread.sleep(2000);
             }
--- client/src/main/java/com/siemag/test/websockets/client/tyrus/TyrusReconnectHandler.java
+++ client/src/main/java/com/siemag/test/websockets/client/tyrus/TyrusReconnectHandler.java
@@ -1,9 +1,12 @@
 package com.siemag.test.websockets.client.tyrus;
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.BooleanType;
 import org.apache.log4j.Logger;
 import org.glassfish.tyrus.client.ClientManager;
 
 import javax.websocket.CloseReason;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author: vuru
@@ -12,14 +15,25 @@ import javax.websocket.CloseReason;
  */
 public class TyrusReconnectHandler extends ClientManager.ReconnectHandler {
 
+
     private Logger logger = Logger.getLogger(Main.class);
+
+
+    public TyrusReconnectHandler() {
+    }
+
+
     @Override
     public boolean onDisconnect(CloseReason closeReason) {
+        logger.error("Do Reconnect "+closeReason.toString());
         return true;
     }
 
     @Override
     public boolean onConnectFailure(Exception exception) {
+        logger.error("Do Reconnect "+exception.getMessage());
         return true;
     }
+
+
 }
