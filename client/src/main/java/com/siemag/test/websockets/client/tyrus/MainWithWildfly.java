ca723e855d6470348f9a8b515740a22309e2ea77
suikast42 <S_Vurucu@hotmail.com>
2014-07-25 11:16:58 +0200

    Girzly only.

--- client/src/main/java/com/siemag/test/websockets/client/tyrus/MainWithWildfly.java
+++ client/src/main/java/com/siemag/test/websockets/client/tyrus/MainWithWildfly.java
@@ -2,24 +2,18 @@ package com.siemag.test.websockets.client.tyrus;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
-import org.glassfish.tyrus.client.ClientManager;
 
-import javax.websocket.*;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 /**
  * @author: vuru
  * Date: 08.04.2014
  * Time: 15:03
  */
-public class MainV2 {
+public class MainWithWildfly {
 
     //    http://www.programmingforliving.com/2013/08/jsr-356-java-api-for-websocket-client-api.html
-    private static Logger logger = Logger.getLogger(MainV2.class);
+    private static Logger logger = Logger.getLogger(MainWithWildfly.class);
     private static String url1 = "ws://localhost:8080/server/hello/1";
     private static String url2 = "ws://localhost:8080/server/hello/2";
     private static String url3 = "ws://localhost:8080/server/hello/3";
